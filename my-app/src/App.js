dba3eeb76f7313825336d5bca7992857ec8e24ee
JarlVPN <Jarlvpn@protonmail.com>
2022-04-13 23:50:47 +0200

    big update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import './x.js'
+import './images/chuj.svg'
+import './Animation.css'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          chuj <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Nav">
+       <div className="d-flex flex-row mb-3 fixed">
+       <a href="roxy.pl"><div className="logo"><p className='text'></p></div></a>
+       <a href="roxy.pl"><div className="p-2"><p className='text'>Home</p></div></a>
+       <a href="roxy.pl"><div className="p-2"><p className='text'>Galery</p></div></a>
+       <a href="roxy.pl"><div className="p-2"><p className='text'>Contact</p></div></a>
+       <a href="roxy.pl"><div className="p-2"><p className='text'>TOS</p></div></a>
+        
+      </div>
+      
+    <div className='yes'>
+    <a href="roxy.pl"><div className="p-2 text2 "><p className='text'>Get Fucked</p></div></a>
+    
+      </div>
+      
+
+    <div className='text-focus-in text element1  '>
+    <div className='element2'><h1 className='uptext'>#TITLE</h1></div>
+    <div className='element3'><h1 className='text'>Mahon jest jebany w dupe</h1></div>
+    <div className='element4 zawijka'><p>kocham prezydenta dude i morawieckiego kocham prezydenta dude i morawieckiego kocham prezydenta dude i morawieckiego kocham prezydenta dude i morawieckiego</p></div>
+
+    <div className='text-focus-in text'>
+    <div className='features img'>twoja mamuśka</div>
+    <div className='features img'>jest jebana</div>
+    <div className='features img'>bo cukierki chciala</div>
+    </div>
+
+
+    <div className='pis'>
+    <div className='element2'><h1 className='uptext'>#Jebanie Teściowej</h1></div>
+    <div className='element3'><h1 className='text'>Mahon jest jebany w dupe</h1></div>
+    <div className='element4 zawijka'><p>Usługi ogrodniczo porządkowe marki krzyśUsługi ogrodniczo porządkowe marki krzyśUsługi ogrodniczo porządkowe marki krzyśUsługi ogrodniczo porządkowe marki krzyśUsługi ogrodniczo porządkowe marki krzyśUsługi ogrodniczo porządkowe marki krzyśUsługi ogrodniczo porządkowe marki krzyś 
+      </p></div>
+
+    <div className='text-focus-in text'>
+
+      
+
+    </div>
+
+      
+    </div>
+
+      
+    </div>
+      <div className='XDD'></div>
+      <div className='Jarl'></div>
+      <div className='Down'></div>
+      <div className='animationjarl'><div className='logo1'> </div></div>
+
       </header>
     </div>
   );
 }
+
+const UnderMaintenance = () =>
+    <div className="chuj">
+      <span style={{color: "white"}}>Website is under maintenance :/ We will restore normal activity today :)</span>
+    </div>
 
 export default App;
---
+++ my-app/src/x.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function x() {
+    return (
+      <div className="text">
+          <p>1x</p>
+      </div>
+    );
+  }
+
+export default x;
